9b2091c873e5c31f76b4d16893b888496bc4f7ab
sg003 <sahanaghb@gmail.com>
2023-12-29 23:54:16 +0530

    Modified notifications

--- client2/src/components/Notification/studentnotif.jsx
+++ client2/src/components/Notification/studentnotif.jsx
@@ -7,9 +7,10 @@ import {
   PopoverArrow,
   PopoverCloseButton,
   PopoverBody,
-  PopoverHeader
+  PopoverHeader,
+  Textarea
 } from "@chakra-ui/react";
-import { BellIcon } from "@chakra-ui/icons";
+import { AddIcon, BellIcon } from "@chakra-ui/icons";
 import axios from 'axios';
 import { useTheme } from '../../Global/ThemeContext';
 import { SkeletonLoader } from "../../Global/SkeletonLoader";
@@ -23,11 +24,17 @@ export default function StudentNotification() {
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
   const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [task, setTask] = useState("");
+  const [canPost, setCanPost] = useState(false);
 
   const getNotifs = async (page) => {
     setLoading(true);
     try {
       const user = await getUserDetails();
+      if(user.role == 'ADMIN' || user.role == 'COORDINATOR'){
+        setCanPost(true);
+      }
       const res = await axios.get(`${url}/announcements/all?department=${user.department}&page=${page}&limit=5`);
       setNotifications(res.data.data);
       setTotalPages(Math.min(Math.ceil(res.data.count / 5), 30));
@@ -76,7 +83,17 @@ export default function StudentNotification() {
           <PopoverContent bg={colors.secondary} minW={'40vw'}>
             <PopoverArrow />
             <PopoverCloseButton />
-            <PopoverHeader textAlign="left" bg={colors.secondary} color={colors.font}>Notifications</PopoverHeader>
+            <PopoverHeader textAlign="left" bg={colors.secondary} color={colors.font}>
+              Notifications
+              {canPost ? (<Button
+                onClick={() => {
+                  setShowModal(true);
+                }}
+              >
+                <AddIcon boxSize={4} />
+              </Button>
+              ) :null}
+            </PopoverHeader>
             <PopoverBody textAlign="left" bg={colors.secondary}>
               {renderNotifications()}
               <div>
@@ -85,6 +102,58 @@ export default function StudentNotification() {
             </PopoverBody>
           </PopoverContent>
         </Popover>
+        {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    Announcement Section:
+                  </h3>
+                  <div className="ml-10">
+                    <Button
+                      onClick={() => setShowModal(false)}
+                      variant="unstyled"
+                    >
+                      <span className="text-red-700">x</span>
+                    </Button>
+                  </div>
+                </div>
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
+                    <form>
+                      <div className="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-200 dark:border-gray-700">
+                        <Textarea
+                          id="comment"
+                          rows="6"
+                          value={task}
+                          onChange={(e) => setTask(e.target.value)}
+                          placeholder="Write a comment..."
+                          required
+                          textAlign="left"
+                        />
+                      </div>
+                    </form>
+                  </p>
+                </div>
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <Button
+                    onClick={() => setShowModal(false)}
+                    bgColor="red.500"
+                    _hover={{ bgColor: "red.800" }}
+                    color="white"
+                    size="sm"
+                  >
+                    Post
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
       </div>
     </>
   );
